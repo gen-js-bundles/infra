e7e34404ea98ab3d15fa551cb783db1ed33edf98
Ludovic Chaboud <l.chaboud@gmail.com>
2015-02-17 14:32:50 +0100

    model

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# infra
-Infra
+# Infra
+
+# Vagrant
+
+* ```server.yml```
+
+```
+ip: 192.168.29.101
+memory: 1536
+
+tags:
+  vagrant:
+    sh:
+      - install:
+          - nano
+          - locate
+          - docker
+          - fig
+```
+
+# Docker
+
+## Tomcat
+* ```tomcat.yml``` :
+
+```
+server: server
+
+tags:
+  docker:
+    from: ubuntu
+    run:
+      install:
+        - tomcat
+    volume:
+      - /target
+    expose:
+      - 8080: 8080
+      - 22
+    links:
+      mongodb
+    cmd:
+      [ "/opt/tomcat/bin/catalina.sh" ]
+```
+
+## MongoDB
+* ```mongodb```
+
+```
+server: server
+
+tags:
+  docker:
+    from: ubuntu
+    run:
+      install:
+        - mongodb
+    cmd:
+      service mongod start
+```
--- install/tomcat.sh
+++ install/tomcat.sh
@@ -1,6 +1,6 @@
 # Apache Tomcat $__TOMCAT_MINOR_VERSION__$
-wget -q https://archive.apache.org/dist/tomcat/tomcat-$__TOMCAT_MAJOR_VERSION__$/v$__TOMCAT_MINOR_VERSION__$/bin/apache-tomcat-$__TOMCAT_MINOR_VERSION__$.tar.gz
-wget -qO- https://archive.apache.org/dist/tomcat/tomcat-$__TOMCAT_MAJOR_VERSION__$/v$__TOMCAT_MINOR_VERSION__$/bin/apache-tomcat-$__TOMCAT_MINOR_VERSION__$.tar.gz.md5 | md5sum -c - 
-tar zxf apache-tomcat-*.tar.gz
-rm apache-tomcat-*.tar.gz
-mv apache-tomcat* tomcat
+sudo wget -q https://archive.apache.org/dist/tomcat/tomcat-$__TOMCAT_MAJOR_VERSION__$/v$__TOMCAT_MINOR_VERSION__$/bin/apache-tomcat-$__TOMCAT_MINOR_VERSION__$.tar.gz
+sudo wget -qO- https://archive.apache.org/dist/tomcat/tomcat-$__TOMCAT_MAJOR_VERSION__$/v$__TOMCAT_MINOR_VERSION__$/bin/apache-tomcat-$__TOMCAT_MINOR_VERSION__$.tar.gz.md5 | md5sum -c -
+sudo tar zxf apache-tomcat-*.tar.gz
+sudo rm apache-tomcat-*.tar.gz
+sudo ln -s apache-tomcat* tomcat
