1c334fc6fe861bf882f2a6137f74b6cc997c0c06
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-20 17:47:10 +0100

    Update jenkins.sh

--- install/jenkins.sh
+++ install/jenkins.sh
@@ -6,11 +6,7 @@ sudo apt-get install -y jenkins jenkins-cli
 
 # wait restart
 sudo service jenkins restart
-#while netstat -lnt | awk '$4 ~ /:8080$/ {exit 1}'; do sleep 10; done
-#curl -sL -w "%{http_code}" "http://localhost:8080" -o /dev/null
-#result=`curl -sL -w "%{http_code}" "http://localhost:8080" -o /dev/null`
-#if [[ `curl -sL -w "%{http_code}" "http://localhost:8080" -o /dev/null` == 200 ]] then echo "toto" fi;
-while [[ `curl -sL -w "%{http_code}" "http://localhost:8080" -o /dev/null` != 200 ]]; do sleep 10; done
+while [[ `curl -sL -w "%{http_code}" "http://localhost:8080" -o /dev/null` != 200 ]]; do sleep 50; done
 
 # install plugin
 sudo jenkins-cli -s http://localhost:8080 install-plugin docker-build-step
