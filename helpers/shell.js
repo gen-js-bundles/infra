fe476b9a93caa314fb89f667c400eb393beb4585
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-20 14:40:50 +0100

    Update shell.js

--- helpers/shell.js
+++ helpers/shell.js
@@ -133,6 +133,10 @@ module.exports = (function Shell() {
             this.installOne('java', 'java');
             this.out.println(this.read('maven.sh'));
         }
+        else if(eltName === 'jenkins') {
+            this.installOne('apache2', 'apache2');
+            this.out.println(this.read('jenkins.sh'));
+        }
         else if(eltName === 'mongodb') {
             var data = {};
             if(elt != null && elt != 'mongodb') {
