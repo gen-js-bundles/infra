042fceee2e4aa4620a26fc3feb2c7422a9205e16
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-20 14:58:09 +0100

    Update jenkins.sh

--- install/jenkins.sh
+++ install/jenkins.sh
@@ -2,7 +2,7 @@
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update -y
-sudo apt-get install -y jenkins
+sudo apt-get install -y jenkins jenkins-cli
 
 # update jenkins
 wget -q http://mirrors.jenkins-ci.org/war/latest/jenkins.war /usr/share/jenkins/jenkins.war
