bb428c2088c36c29ba7ba295a1f97f455c74e2b1
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-20 15:51:01 +0100

    Update init.js

--- tasks/init.js
+++ tasks/init.js
@@ -80,7 +80,7 @@ module.exports = {
       var data = {
         sh: [
           {
-            purge: 'openjdk-\*'
+            purge: ['openjdk-\*']
           },
           {
             install: answers.install
