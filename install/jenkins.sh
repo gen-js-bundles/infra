9353542e9b2d4f8507b6dfb75aec69fbfca799c4
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-20 14:52:12 +0100

    Update jenkins.sh

--- install/jenkins.sh
+++ install/jenkins.sh
@@ -1,10 +1,11 @@
+# jenkins
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y
+sudo apt-get install -y jenkins
 
 # update jenkins
-wget -q http://mirrors.jenkins-ci.org/war/latest/jenkins.war /usr/share/jenkins/jenkins.war
+wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war /usr/share/jenkins/jenkins.war
 
 # install plugin
 jenkins-cli -s http://localhost:8080 install-plugin docker-build-step
